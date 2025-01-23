2d3329ad639ed6aba076e29ccc7356421b929007
Akshansh Gusain <akshanshgusain@Akshanshs-MacBook-Air.local>
2025-01-23 22:44:56 +0530

    feat: add Token data structure and TokenTypes

---
+++ token/token.go
@@ -0,0 +1,50 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType //type of token (e.g., string, ', ;, float, etc)
+	Literal string    // literal value of the token
+}
+
+// list of all supported TokenTypes:
+const (
+	ILLEGAL = "ILLEGAL" // unsupported TokenType
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
