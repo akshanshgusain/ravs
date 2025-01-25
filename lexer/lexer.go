dc5e4678fe7d963d13a12d5c32fa4eea37414596
akshanshgusain <akshanshgusain@gmail.com>
2025-01-25 17:40:14 +0530

    feat: add NextToken method to convert char to Token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "ravs_lang/token"
+
 // Lexer readPosition always points to the next position where we're going to read from next
 // position always points to the position where we last read
 
@@ -25,4 +27,56 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// NextToken returns Token based on the current character
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+	switch l.currentChar {
+	case '=':
+		t = token.Token{Type: token.ASSIGN, Literal: string(l.currentChar)}
+	case '/':
+		t = token.Token{Type: token.SLASH, Literal: string(l.currentChar)}
+	case '*':
+		t = token.Token{Type: token.ASTERISK, Literal: string(l.currentChar)}
+	case '<':
+		t = token.Token{Type: token.LT, Literal: string(l.currentChar)}
+	case '>':
+		t = token.Token{Type: token.GT, Literal: string(l.currentChar)}
+	case ';':
+		t = token.Token{Type: token.SEMICOLON, Literal: string(l.currentChar)}
+	case ',':
+		t = token.Token{Type: token.COMMA, Literal: string(l.currentChar)}
+	case '{':
+		t = token.Token{Type: token.LBRACE, Literal: string(l.currentChar)}
+	case '}':
+		t = token.Token{Type: token.RBRACE, Literal: string(l.currentChar)}
+	case '(':
+		t = token.Token{Type: token.LPAREN, Literal: string(l.currentChar)}
+	case ')':
+		t = token.Token{Type: token.RPAREN, Literal: string(l.currentChar)}
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	default:
+		if isLetter(l.currentChar) {
+			t.Literal = l.readIdentifier()
+			return t
+		} else {
+			t = token.Token{Type: token.ILLEGAL, Literal: string(l.currentChar)}
+		}
+	}
+	return t
+}
+
+func (l *Lexer) readIdentifier() string {
+	p := l.position
+	for isLetter(l.currentChar) {
+		l.readChar()
+	}
+	return l.input[p:l.position]
+}
+
+func isLetter(c byte) bool {
+	return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_'
 }
