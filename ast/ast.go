8f1b28b6478aa011321549b43b6f18199582a882
akshanshgusain <akshanshgusain@gmail.com>
2025-01-30 23:22:00 +0530

    feat: add LetStatement and Identifer to the AST

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "ravs_lang/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -13,3 +15,37 @@ type Expression interface {
 	Node // Embedding Node, composition of Node interface
 	expressionNode()
 }
+
+// Root node
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // Token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+func (ls *LetStatement) statementNode() {}
+
+type Identifier struct {
+	Token token.Token // Token.IDENT
+	Value string
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *Identifier) expressionNode() {}
