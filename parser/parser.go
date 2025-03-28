fd53a209e44ab84dfd27151eb03ca6e07c63990c
akshanshgusain <akshanshgusain@gmail.com>
2025-03-28 06:39:55 +0530

    feat: add error handling to parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"ravs_lang/ast"
 	"ravs_lang/lexer"
 	"ravs_lang/token"
@@ -11,10 +12,15 @@ type Parser struct {
 	l         *lexer.Lexer
 	currToken token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
 	// read two tokens, so currToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -41,6 +47,15 @@ func (p *Parser) ParseProgram() *ast.Program {
 	return program
 }
 
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
@@ -51,10 +66,16 @@ func (p *Parser) parseStatement() ast.Statement {
 }
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.currToken}
+
+	// expects an identifier
 	if !p.expectPeek(token.IDENT) {
 		return nil
 	}
+
+	// create an identifier node
 	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	// expects an equal sign
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
@@ -80,6 +101,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
