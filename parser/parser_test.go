eff0e25c643b815ea509144a2a8f69da3945e6c5
akshanshgusain <akshanshgusain@gmail.com>
2025-04-08 10:33:10 +0530

    test(parser): add test for integer literal

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -128,3 +128,34 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Errorf("ident.TokenLiteral not 'foobar'. got=%s", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerLiteral(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	var program *ast.Program = p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program does not have enough statements. got=%d", len(program.Statements))
+	}
+	// type assertion
+	// value := myInterface.(ConcreteType)
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("literal.Value not %d. got=%d", 5, literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.ToeknLiteral not %s. got=%s", "5", literal.TokenLiteral())
+	}
+}
